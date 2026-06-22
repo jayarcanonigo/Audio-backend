3703fc77d3e1cf23c2d188bcfc2f23fb090fe480
jayarcanonigo <31925918+jayarcanonigo@users.noreply.github.com>
2026-06-22 16:00:54 +0800

    Change WhisperModel from 'small' to 'base'

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def create_progress():
 @app.on_event("startup")
 def load_model():
     global model
-    model = WhisperModel("small", device="cpu", compute_type="int8")
+    model = WhisperModel("base", device="cpu", compute_type="int8")
     print("Whisper loaded")
 
 
